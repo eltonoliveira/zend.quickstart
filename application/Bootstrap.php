625ab2c1bb1d973e6c241c3b654ceaece4d0aea7
Elton Oliveira <elton@tmax.com.br>
2012-07-31 00:09:14 -0300

    2: Adicionado o doctype no boostrap e no layout

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,48 @@
 <?php
-
+/**
+ * Classe de bootstrap.
+ * 
+ * @tutorial 
+ * 
+ *
+ * @author Elton D. de Oliveira
+ * @category Application
+ * @package Application   
+ * @name Bootstrap
+ * @access public
+ * @version 0.0.1
+ * @license GPL (http://www.fsf.org/)
+ * @copyright copyleft
+ *
+ */
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+	/** -------------------------------------------------------------------------------------------------------------------------- */
+	
+	/**
+	 * Configura o doctype do sistema
+	 * 
+	 * <code>
+	 * <?php
+	 * 
+	 * ?>	
+	 * </code>
+	 * 
+	 * @tutorial Configura o doctype do sistema
+	 * 
+	 * @author Elton D. de Oliveira
+	 * @name _initDocType
+	 * @access protected
+	 * @return void
+	 * 
+	 */
+	protected function _initDocType()
+	{
+		$this->bootstrap('view');
+		$view = $this->getResource('view');
+		$view->doctype('XHTML1_STRICT');
+	}
 
-
+	/** -------------------------------------------------------------------------------------------------------------------------- */
 }
 
